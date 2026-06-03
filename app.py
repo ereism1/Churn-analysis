4a43486b84e46ecd52d40817742f3c7b4c4df598
EduardoReis <ereism1@outlook.com>
2026-06-03 03:14:24 -0300

    adição de gráficos

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import joblib
 import pandas as pd 
 import seaborn as sns
 import matplotlib.pyplot as plt
+import plotly.express as px
 
 
 
@@ -15,116 +16,148 @@ df = pd.read_excel(
     "data/Telco_customer_churn.xlsx"
 )
 
-st.title("Predição de Churn")
-
-tenure = st.number_input(
-    "Tempo de permanência (meses)",
-    min_value=0,
-    max_value=72,
-    value=12
-)
-
-monthly_charges = st.number_input(
-    "Mensalidade",
-    min_value=0.0,
-    value=50.0
-)
-
-total_charges = st.number_input(
-    "Valor total gasto",
-    min_value=0.0,
-    value=500.0
-)
-
-contract = st.selectbox(
-    "Tipo de contrato",
+aba1, aba2 = st.tabs(
     [
-        "Month-to-month",
-        "One year",
-        "Two year"
+        
+        "🎯 Predição",
+        "📊 Dashboard"
     ]
 )
 
-dependents = st.selectbox(
-    "Possui dependentes?",
-    ["No", "Yes"]
-)
+with aba2:
+    st.subheader("Fatores de Churn")
+    
 
-tech_support = st.selectbox(
-    "Possui suporte técnico?",
-    ["No", "Yes"]
-)
+    col1, col2, col3 = st.columns(3)
 
-online_security = st.selectbox(
-    "Possui segurança online?",
-    ["No", "Yes"]
-)
+    with col1:
+        st.metric("Clientes", len(df))
 
-payment_method = st.selectbox(
-    "Método de pagamento",
-    [
-        "Bank transfer (automatic)",
-        "Credit card (automatic)",
-        "Electronic check",
-        "Mailed check"
-    ]
-)
+    with col2:
+        st.metric(
+            "Taxa de Churn",
+            f"{df['Churn Value'].mean()*100:.2f}%"
+        )
 
-if st.button("Prever Churn"):
+    with col3:
+        st.metric(
+            "Mensalidade Média",
+            f"${df['Monthly Charges'].mean():.2f}"
+        )
+    
 
-    dados = pd.DataFrame(
-        0,
-        index=[0],
-        columns=colunas
+    
+
+    fig_pizza = px.pie(
+        df,
+        names="Churn Label",
+        title="Distribuição de Churn"
     )
 
-    # preencher dados...
-
-    dados_escalados = scaler.transform(dados)
-
-    probabilidade = modelo.predict_proba(
-        dados_escalados
-    )[0][1]
-
-    prob = probabilidade * 100
-
-    st.subheader("Resultado")
-
-    st.metric(
-        "Probabilidade de Churn",
-        f"{prob:.2f}%"
+    fig_contrato = px.histogram(
+    df,
+    x="Contract",
+    color="Churn Label"
     )
 
-    st.progress(prob / 100)
+    col1, col2 = st.columns(2)
 
-    if prob >= 70:
-        st.error("⚠️ Alto risco de cancelamento")
+    with col1:
+        st.plotly_chart(fig_pizza)
 
-    elif prob >= 40:
-        st.warning("⚡ Risco moderado")
+    with col2:
+        st.plotly_chart(fig_contrato)
 
-    else:
-        st.success("✅ Baixo risco")
+    
 
+with aba1:
+    st.title("Predição de Churn")
 
+    tenure = st.number_input(
+        "Tempo de permanência (meses)",
+        min_value=0,
+        max_value=72,
+        value=12
+    )
 
-st.header("Fatores de Churn")
+    monthly_charges = st.number_input(
+        "Mensalidade",
+        min_value=0.0,
+        value=50.0
+    )
 
-top_fatores = pd.DataFrame({
-    "Variável":[
-        "Total Charges",
-        "Fiber Optic",
-        "Paperless Billing",
-        "Partner"
-    ],
-    "Impacto":[
-        0.72,
-        0.27,
-        0.19,
-        0.15
-    ]
-})
+    total_charges = st.number_input(
+        "Valor total gasto",
+        min_value=0.0,
+        value=500.0
+    )
 
-st.bar_chart(
-    top_fatores.set_index("Variável")
-)
+    contract = st.selectbox(
+        "Tipo de contrato",
+        [
+            "Month-to-month",
+            "One year",
+            "Two year"
+        ]
+    )
+
+    dependents = st.selectbox(
+        "Possui dependentes?",
+        ["No", "Yes"]
+    )
+
+    tech_support = st.selectbox(
+        "Possui suporte técnico?",
+        ["No", "Yes"]
+    )
+
+    online_security = st.selectbox(
+        "Possui segurança online?",
+        ["No", "Yes"]
+    )
+
+    payment_method = st.selectbox(
+        "Método de pagamento",
+        [
+            "Bank transfer (automatic)",
+            "Credit card (automatic)",
+            "Electronic check",
+            "Mailed check"
+        ]
+    )
+
+    if st.button("Prever Churn"):
+
+        dados = pd.DataFrame(
+            0,
+            index=[0],
+            columns=colunas
+        )
+
+        # preencher dados...
+
+        dados_escalados = scaler.transform(dados)
+
+        probabilidade = modelo.predict_proba(
+            dados_escalados
+        )[0][1]
+
+        prob = probabilidade * 100
+
+        st.subheader("Resultado")
+
+        st.metric(
+            "Probabilidade de Churn",
+            f"{prob:.2f}%"
+        )
+
+        st.progress(prob / 100)
+
+        if prob >= 70:
+            st.error("⚠️ Alto risco de cancelamento")
+
+        elif prob >= 40:
+            st.warning("⚡ Risco moderado")
+
+        else:
+            st.success("✅ Baixo risco")
